16750ce8bee66fed6251c6e81ee61fc319fac9f1
Superloup10 <Superloup10@gmail.com>
2024-04-02 15:16:06 +0200

    Add Jest testing and update package.json
    Implemented Jest configuration for application testing. Added Jest-related devDependencies in the package.json, and specified new scripts for running tests. Also implemented initial tests and snapshots for the home page.

---
+++ __tests__/snapshot.tsx
@@ -0,0 +1,8 @@
+import "@testing-library/jest-dom";
+import {render} from "@testing-library/react";
+import Home from "@/app/page";
+
+it("renders homepage unchanged", () => {
+    const {container} = render(<Home/>);
+    expect(container).toMatchSnapshot();
+});
