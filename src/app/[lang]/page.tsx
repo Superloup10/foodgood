7b0641f6a6e79ac07845f6b101a48cedb0468202
Superloup10 <Superloup10@gmail.com>
2024-04-09 12:15:26 +0200

    Refactor code and fix image link bugs
    This commit simplifies and refactors several blocks of code to be more readable and maintainable. This change primarily includes restructuring the styling and logic of many components. Alongside this, an issue regarding incorrect image links has also been fixed. Multiple unused imports have also been removed for better code hygiene and clarity.

--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -2,79 +2,77 @@
 
 import Link from "next/link";
 import Image from "next/image";
-import { Category } from "@/domain/model/category";
 import useFetch from "@/hooks/useFetch";
-import { Product } from "@/domain/model/product.dto";
+import {Product} from "@/domain/model/product.dto";
 import {
-  Carousel,
-  CarouselContent,
-  CarouselItem,
-  CarouselNext,
-  CarouselPrevious,
+    Carousel,
+    CarouselContent,
+    CarouselItem,
+    CarouselNext,
+    CarouselPrevious,
 } from "@/components/ui/carousel";
 import Autoplay from "embla-carousel-autoplay";
 import Reseau from "@/components/Reseau";
 
 export default function Home() {
-  const { data: products } = useFetch<Product[]>("/api/v1/products");
-  function getProduitsAleatoires(): Product[] {
-    const produitAleatoires: Product[] = [];
-    const nombreProduits = products!.length;
-    while (produitAleatoires.length < 30) {
-      const index = Math.floor(Math.random() * nombreProduits);
-      if (!produitAleatoires.includes(products![index])) {
-        produitAleatoires.push(products![index]);
-      }
+    const {data: products} = useFetch<Product[]>("/api/v1/products");
+
+    function getProduitsAleatoires(): Product[] {
+        const produitAleatoires: Product[] = [];
+        const nombreProduits = products!.length;
+        while (produitAleatoires.length < 30) {
+            const index = Math.floor(Math.random() * nombreProduits);
+            if (!produitAleatoires.includes(products![index])) {
+                produitAleatoires.push(products![index]);
+            }
+        }
+        return produitAleatoires;
     }
-    return produitAleatoires;
-  }
 
-  return (
-    <main className="px-10 ">
-      
+    return (
+        <main className="px-10 ">
+            <div className="flex justify-justify-between gap-96">
+                <div className="mt-36">
+                    <Carousel
+                        opts={{align: "start", loop: true}}
+                        className="w-full"
+                        plugins={[
+                            Autoplay({
+                                delay: 1000,
+                            }),
+                        ]}
+                    >
+                        <CarouselContent className="-ml-4 flex items-center">
+                            {products &&
+                                getProduitsAleatoires().map((product) => (
+                                    <CarouselItem key={product.id} className="pl-4 basis-1/5">
+                                        <Link href={`/product/${product.name}`}>
+                                            <Image
 
-      <div className="flex justify-justify-between gap-96">
-        <div className="  mt-36">
-          <Carousel
-            opts={{ align: "start", loop: true }}
-            className="w-full"
-            plugins={[
-              Autoplay({
-                delay: 1000,
-              }),
-            ]}
-          >
-            <CarouselContent className="-ml-4 flex items-center">
-              {products &&
-                getProduitsAleatoires().map((product) => (
-                  <CarouselItem key={product.id} className="pl-4 basis-1/5">
-                    <Link href={`/product/${product.name}`}>
+                                                src={product.image}
+                                                alt={product.name}
+                                                width={200}
+                                                height={1}
+                                            />
+                                        </Link>
+                                    </CarouselItem>
+                                ))}
+                        </CarouselContent>
+                        <CarouselPrevious/>
+                        <CarouselNext/>
+                    </Carousel>
+                </div>
+                <div className="mt-5">
+
                     <Image
-                      
-                      src={product.image}
-                      alt={product.name}
-                      width={200}
-                      height={1}
+                        src="/images/caddie.jpg"
+                        alt="chariot"
+                        width={3000}
+                        height={1}
                     />
-                    </Link>
-                  </CarouselItem>
-                ))}
-            </CarouselContent>
-            <CarouselPrevious />
-            <CarouselNext />
-          </Carousel>
-        </div>
-        <div className="mt-5">
-          
-          <Image
-            src="/image/caddie.jpg"
-            alt="chariot"
-            width={3000}
-            height={1}
-          />
-        </div>
-      </div>
-      <Reseau/>
-    </main>
-  );
+                </div>
+            </div>
+            <Reseau/>
+        </main>
+    );
 }
--- src/components/Categorie/index.tsx
+++ src/components/Categorie/index.tsx
@@ -1,25 +1,25 @@
 import Link from "next/link";
 import Image from "next/image";
-import { Category } from "@/domain/model/category";
+import {Category} from "@/domain/model/category";
 
-export default function Categorie(){
+export default function Categorie() {
 
-  return(
-    <div className="flex items-center justify-center h-fit sm:gap-5 sm:p-6  ">
-        {Object.values(Category).map((value, index) => (
-          <Link  key={index} href={`/${value.toLowerCase()}`} className="flex flex-col items-center justify-center">
-            <Image
-             
-              src={`/image/${value.toLowerCase()}.jpg`}
-              className="rounded-full"
-              alt={value.toLowerCase()}
-              width={75}
-              height={75}
-            />
-            <p className="flex justify-center">{value.toLowerCase().replace("_", " ")}</p>
-          </Link>
-        ))}
-      </div>
+    return (
+        <div className="flex items-center justify-center h-fit sm:gap-5 sm:p-6  ">
+            {Object.values(Category).map((value, index) => (
+                <Link key={index} href={`/${value.toLowerCase()}`}
+                      className="flex flex-col items-center justify-center">
+                    <Image
+                        src={`/images/${value.toLowerCase()}.jpg`}
+                        className="rounded-full"
+                        alt={value.toLowerCase()}
+                        width={75}
+                        height={75}
+                    />
+                    <p className="flex justify-center">{value.toLowerCase().replace("_", " ")}</p>
+                </Link>
+            ))}
+        </div>
 
-  )
+    )
 }
