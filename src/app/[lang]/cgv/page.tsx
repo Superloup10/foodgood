154f853b8dd1e12e6b55f8a5363c8e01f5639b3e
screddy972@ <boycel972@hotmail.fr>
2024-04-18 08:52:09 +0200

    mise a jour add cgv

---
+++ src/app/[lang]/cgv/page.tsx
@@ -0,0 +1,35 @@
+export default function Cgv() {
+  return (
+    <div className="mt-11">
+      <h1 className="mt-11 text-5xl">Conditions Générales de Vente</h1>
+      <h2 className="mt-11 text-3xl">
+        Conditions Générales de Vente F-COMMERCE
+      </h2>
+      <h3 className="mt-11">PARTIE 1</h3>
+      <h4 className="mt-11">LES PARCOURS DE VENTE SUR LE SITE FOODGOOD</h4>
+      <div>
+        <p className="mt-11">ARTICLE 1.</p>
+        <p className="mt-11">
+          DEFINITION Dans les présentes conditions générales de vente, les
+          termes employés ci-après avec une majuscule ont la définition suivante
+          : Centre FOODGOOD : tout hypermarché, supermarché et/ou magasin
+          spécialisé physique exploité sous enseigne FOODGOOD qui propose des
+          produits à la vente via le service "Click&Collect" sur le Site Client
+          : personne physique ou morale souhaitant effectuer un achat sur le
+          Site pour un usage final des produits Conditions Particulières
+          Vendeurs : les Conditions Particulières de Vente du Vendeur Tiers CGV
+          : les présentes conditions générales de vente FCOMMERCE : société
+          proposant des produits à la vente sur le site qu'elle exploite, telle
+          qu'identifiée ci-dessous. Marketplace : espace sur le Site réservé aux
+          Vendeurs Tiers sur lequel ces derniers proposent des offres Offre :
+          ensemble d'informations précontractuelles liées aux produits proposés
+          à la vente sur le Site et notamment les informations liées au prix,
+          stocks disponibles, types de produit et délais de livraison. Site :
+          site e-commerce www.foodgood.com Vendeurs Tiers : vendeurs
+          professionnels proposant des produits à la vente sur la Marketplace du
+          Site
+        </p>
+      </div>
+    </div>
+  );
+}
--- src/components/Cart/index.tsx
+++ src/components/Cart/index.tsx
@@ -1,73 +1,69 @@
 "use client";
-import {useCart} from "@/context/CartContext";
-import {Button} from "../ui/button";
-import {SheetTrigger, SheetContent, Sheet} from "../ui/sheet";
+import { useCart } from "@/context/CartContext";
+import { Button } from "../ui/button";
+import { SheetTrigger, SheetContent, Sheet } from "../ui/sheet";
 
 import BoutonAjouterPanier from "../BoutonAjouterPanier";
 import BoutonSupprimerProduit from "../BoutonSupprimerProduit";
-import BoutonSupprimerPanier from "../BouttonSupprimmerPanier";
-import {useRouter} from "next/navigation";
+import BoutonSupprimerPanier from "../BouttonSupprimerPanier";
+import { useRouter } from "next/navigation";
 
 export default function Cart() {
-    const {cart, totalPrice} = useCart();
-    const router = useRouter();
+  const { cart, totalPrice } = useCart();
+  const router = useRouter();
 
-    const validateCart = () => {
-        if (cart && cart.length > 0) {
-            router.push("/cart");
-        }
-    };
+  const validateCart = () => {
+    if (cart && cart.length > 0) {
+      router.push("/cart");
+    }
+  };
 
-    return (
-        <Sheet>
-            <SheetTrigger asChild>
-                <Button variant="ghost" size="icon">
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 24 24"
-                        fill="currentColor"
-                        className="w-6 h-6"
-                    >
-                        <path
-                            d="M2.25 2.25a.75.75 0 0 0 0 1.5h1.386c.17 0 .318.114.362.278l2.558 9.592a3.752 3.752 0 0 0-2.806 3.63c0 .414.336.75.75.75h15.75a.75.75 0 0 0 0-1.5H5.378A2.25 2.25 0 0 1 7.5 15h11.218a.75.75 0 0 0 .674-.421 60.358 60.358 0 0 0 2.96-7.228.75.75 0 0 0-.525-.965A60.864 60.864 0 0 0 5.68 4.509l-.232-.867A1.875 1.875 0 0 0 3.636 2.25H2.25ZM3.75 20.25a1.5 1.5 0 1 1 3 0 1.5 1.5 0 0 1-3 0ZM16.5 20.25a1.5 1.5 0 1 1 3 0 1.5 1.5 0 0 1-3 0Z"/>
-                    </svg>
-                </Button>
-            </SheetTrigger>
-            <SheetContent className="flex flex-col h-screen">
-                <div className="   flex-1 overflow-auto p-4 bg-cadie bg-contain bg-no-repeat bg-center  ">
-                    <h1 className="flex justify-center font-bold text-2xl bg-customGreen border-4 border-transparent shadow-[2px_2px_0_1px] rounded-lg">
-                        MON PANIER
-                    </h1>
-                    <div className="flex-1 mt-5 overflow-y-auto">
-                        {cart &&
-                            cart.map((item) => (
-                                <div
-                                    key={item.product.id}
-                                    className="mb-4 flex items-center justify-between"
-                                >
-                                    <div className="font-bold text-lg">
-                                        {item.quantity} x {item.product.name}{" "}
-                                        {(item.quantity * item.product.price).toFixed(2)}€
-                                    </div>
-                                    <div className="flex gap-2">
-                                        <BoutonSupprimerProduit product={item.product}/>
-                                        <BoutonAjouterPanier product={item.product}/>
-                                    </div>
-                                </div>
-                            ))}
-                    </div>
+  return (
+    <Sheet>
+      <SheetTrigger asChild>
+        <Button variant="ghost" size="icon">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="currentColor"
+            className="w-6 h-6"
+          >
+            <path d="M2.25 2.25a.75.75 0 0 0 0 1.5h1.386c.17 0 .318.114.362.278l2.558 9.592a3.752 3.752 0 0 0-2.806 3.63c0 .414.336.75.75.75h15.75a.75.75 0 0 0 0-1.5H5.378A2.25 2.25 0 0 1 7.5 15h11.218a.75.75 0 0 0 .674-.421 60.358 60.358 0 0 0 2.96-7.228.75.75 0 0 0-.525-.965A60.864 60.864 0 0 0 5.68 4.509l-.232-.867A1.875 1.875 0 0 0 3.636 2.25H2.25ZM3.75 20.25a1.5 1.5 0 1 1 3 0 1.5 1.5 0 0 1-3 0ZM16.5 20.25a1.5 1.5 0 1 1 3 0 1.5 1.5 0 0 1-3 0Z" />
+          </svg>
+        </Button>
+      </SheetTrigger>
+      <SheetContent className="flex flex-col h-screen">
+        <div className="   flex-1 overflow-auto p-4 bg-cadie bg-contain bg-no-repeat bg-center  ">
+          <h1 className="flex justify-center font-bold text-2xl bg-customGreen border-4 border-transparent shadow-[2px_2px_0_1px] rounded-lg">
+            MON PANIER
+          </h1>
+          <div className="flex-1 mt-5 overflow-y-auto">
+            {cart &&
+              cart.map((item) => (
+                <div
+                  key={item.product.id}
+                  className="mb-4 flex items-center justify-between"
+                >
+                  <div className="font-bold text-lg">
+                    {item.quantity} x {item.product.name}{" "}
+                    {(item.quantity * item.product.price).toFixed(2)}€
+                  </div>
+                  <div className="flex gap-2">
+                    <BoutonSupprimerProduit product={item.product} />
+                    <BoutonAjouterPanier product={item.product} />
+                  </div>
                 </div>
-                <div className="p-4 bg-white">
-                    <p className="font-bold">
-                        Total :{" "}
-                        {totalPrice.toFixed(2)}€
-                    </p>
-                </div>
-                <div className="flex justify-center">
-                    <Button onClick={validateCart}>Valider mon panier</Button>
-                    <BoutonSupprimerPanier/>
-                </div>
-            </SheetContent>
-        </Sheet>
-    );
+              ))}
+          </div>
+        </div>
+        <div className="p-4 bg-white">
+          <p className="font-bold">Total : {totalPrice.toFixed(2)}€</p>
+        </div>
+        <div className="flex justify-center">
+          <Button onClick={validateCart}>Valider mon panier</Button>
+          <BoutonSupprimerPanier />
+        </div>
+      </SheetContent>
+    </Sheet>
+  );
 }
